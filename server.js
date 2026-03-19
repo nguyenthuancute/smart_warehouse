20149cf2bde61aff83139722cb4b466fe06fcb14
quangthuanthanh0306 <quangthuanthanh0306@gmail.com>
2026-03-19 15:09:54 +0000

    Force update to sync local and remote

--- server.js
+++ server.js
@@ -69,21 +69,15 @@ io.on('connection', (socket) => {
 });
 
 // --- MQTT ---
-const MQTT_HOST = 'ac283ced08d54c199286b8bdb567f195.s1.eu.hivemq.cloud';
-const MQTT_PORT = 8883;
-const MQTT_USER = 'smart_warehouse';
-const MQTT_PASS = 'Thuan@06032006';
+const MQTT_HOST = '127.0.0.1';
+const MQTT_PORT = 1883;
 
-const client = mqtt.connect(`mqtts://${MQTT_HOST}`, {
-    port: MQTT_PORT,
-    username: MQTT_USER,
-    password: MQTT_PASS,
-    protocol: 'mqtts',
-    rejectUnauthorized: true
+const client = mqtt.connect(`mqtt://${MQTT_HOST}`, {
+    port: MQTT_PORT
 });
 
 client.on('connect', () => {
-    console.log('✅ MQTT Connected');
+    console.log('✅ MQTT Local Connected');
     client.subscribe('kho_thong_minh/tags/+');
 });
 
